04a82d65df2ad5353e8bc6a3fd05ff61841a0a04
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-09-10 13:38:16 +0800

    Frontend with react and redux, redux middleware and toast notifications, and react-router, jwt and navbar

--- src/App.js
+++ src/App.js
@@ -1,57 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { React, useEffect } from 'react';
+import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
+import SignInPage from './components/SignInPage';
+import SignUpPage from './components/SignUpPage';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { userAuthenticated } from './app/authenticationSlice';
 
-function App() {
+
+const App = () => {
+  const { isLoggedIn } = useSelector(state => state.authenticationSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    if (token !== undefined && token !== null) {
+      dispatch(userAuthenticated({ token: token }))
+    }
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <HomePage /> : <SignInPage />} />
+        <Route path="/signup" element={isLoggedIn ? <Navigate to='/' /> : <SignUpPage />} />
+        <Route path="/signin" element={isLoggedIn ? <Navigate to='/' /> : <SignInPage />} />
+        <Route path="*" element={<h2>Page not found!</h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import  store  from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
