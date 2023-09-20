480b0a64146b1d1c9e087e943c0df1a4c861cc9a
siruthesiru <silveriogerald@gmail.com>
2023-09-20 15:45:40 +0800

    Attempting to fix conflict; git missed this file

--- src/components/authprops/authImage.js
+++ src/components/authprops/authImage.js
@@ -6,7 +6,7 @@ const AuthImage = () => {
         <div className="hidden md:flex md:w-[30%] lg:w-[40%]">
             <img
                 src={alumnischool}
-                alt="Alumni walking back to school"
+                alt="Alumni walking back to school."
                 className="mb-4 w-full h-full object-cover"
             />
         </div>
