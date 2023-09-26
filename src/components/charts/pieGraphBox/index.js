f4e9098d5cd224a3ebc68a02634ff9072eb2db07
MonicApor <58206744+MonicApor@users.noreply.github.com>
2023-09-27 02:42:10 +0800

    initial ui for dashboard

---
+++ src/components/charts/pieGraphBox/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+
+import './index.scss'
+
+const data = [
+    { course: 'Course A', employmentRate: 80 },
+    { course: 'Course B', employmentRate: 70 },
+    { course: 'Course C', employmentRate: 65 },
+    { course: 'Course D', employmentRate: 90 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
+const PieGraphBox = () => {
+    return (
+        <div className='pieGraphBox'>
+            <div className='infoBox'>
+                <div className='charts'>
+                    <h1 className='text-3xl'>Employment Rate</h1>
+                    <div className='chart'>
+                        <ResponsiveContainer width="99%" height={300}>
+                            <PieChart>
+                                <Tooltip
+                                    contentStyle={{ background: "white", borderRadius: "5px" }}
+                                />
+                                <Pie
+                                    data={data}
+                                    labelLine={false}
+                                    label={renderCustomizedLabel}
+                                    fill="#8884d8"
+                                    dataKey="employmentRate"
+                                    legendType="circle"
+                                >
+                                    {data.map((entry, index) => (
+                                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                    ))}
+                                </Pie>
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
+                <div className='labels'>
+                    <h1 className='text-xl'> Legends</h1>
+                    {data.map((item, index) => (
+                        <div className='label' key={item.course}>
+                            <div className='title'>
+                                <div className='dot' style={{ backgroundColor: COLORS[index % COLORS.length] }} />
+                                <span>{item.course}</span>
+                            </div>
+                            <span>{item.employmentRate}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className='description'>
+                <p>Lorem ipsum this would be a text</p>
+            </div>
+        </div>
+    )
+}
+
+export default PieGraphBox;
