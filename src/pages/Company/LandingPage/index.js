a73ed8debb6256ac35e2ada993826dceefea2acd
MonicApor <58206744+MonicApor@users.noreply.github.com>
2023-11-08 17:21:52 +0800

    added alumni

--- src/pages/Company/LandingPage/index.js
+++ src/pages/Company/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import AnnouncementCard from '../../../components/announcementCard';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetAllAnnouncements, GetCompanyProfile } from '../../../services/company';
