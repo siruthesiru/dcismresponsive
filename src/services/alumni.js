d060d975a6206a7825690670fefdf9f77da3cc43
MonicApor <58206744+MonicApor@users.noreply.github.com>
2023-11-09 14:23:47 +0800

    ,

--- src/services/alumni.js
+++ src/services/alumni.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { toast } from 'react-toastify';
 import { editProfile, editProfileError, getAlumniProfile, getAlumniProfileError, getAnnouncements, getAnnouncementsError, getEvents, getEventsError, setErrorMessage } from "../app/alumniUserSlice";
 
 
