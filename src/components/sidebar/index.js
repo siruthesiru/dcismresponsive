fd775ff7d4f5f4b9d4b4715b6a755ad3d06a27f3
MonicApor <58206744+MonicApor@users.noreply.github.com>
2023-11-20 20:25:08 +0800

    .

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -107,7 +107,7 @@ const Sidebar = ({
 
                             if (
                                 (text === "Add Admin") &&
-                                user?.email !== "admin@aces.com"
+                                user?.email !== "admin@dcismaces.com"
                             ) {
                                 return null;
                             }
